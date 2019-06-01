8d40466000dba5acc6e7767bd08341d3c594da4b
Ludelaplu <lucien.koot@gmail.com>
2019-06-01 14:17:35 +0200

    for loop incl nr function range

--- python_intro.py
+++ python_intro.py
@@ -1,6 +1,3 @@
-def hi(name):
-    print('Hello !! ' + name)
-girls = ['rachel', 'monica', 'annie', 'lola', 'agneta']
-for name in girls:
-    hi(name)
-    print('...maar ook...')
+for i in range(1,77):
+    print(i)
+    
