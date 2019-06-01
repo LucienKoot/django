aa254c7b0708fed3059ab64dca470a2f14d23ffa
Ludelaplu <lucien.koot@gmail.com>
2019-06-01 14:03:04 +0200

    if elif else another one

--- python_intro.py
+++ python_intro.py
@@ -1,10 +1,4 @@
-#own functions
-
 def hi(name):
-    if name == 'Ola':
-        print ('Hoi Ola!')
-    elif name == 'Sandra':
-        print ('How are you Sandra?')
-    else:
-        print('Ik ken je niet')
-hi('Lu')
+    print('Hello ' + name + '!')
+
+hi("Teacher")
