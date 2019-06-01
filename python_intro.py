579bc6f6d27096b38f2e086c5637712929f303e3
Ludelaplu <lucien.koot@gmail.com>
2019-06-01 12:10:34 +0200

    if elif else volume test

--- python_intro.py
+++ python_intro.py
@@ -1,9 +1,11 @@
 #if statements elif else
+volume = 45
 
-name = 'Ali'
-if name == 'Jan':
-    print('Hoi Jan!')
-elif name == 'Truus':
-    print('Hoi Truusje!')
+if volume < 20:
+    print('Het is stil.')
+elif 20 <= volume < 40:
+    print('Achtergrond muxiekje')
+elif 40 <= volume <80:
+    print('Dat is luider!')
 else:
-    print('Hoi anonieme mens')
+    print('Wat voor geluid is dit!!')
