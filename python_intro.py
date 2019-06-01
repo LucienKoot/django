5816efe0dceac0febeee14410aa96310643733d4
Ludelaplu <lucien.koot@gmail.com>
2019-06-01 12:30:19 +0200

    if elif else own code hi

--- python_intro.py
+++ python_intro.py
@@ -1,11 +1,10 @@
-#if statements elif else
-volume = 45
+#own functions
 
-if volume < 20:
-    print('Het is stil.')
-elif 20 <= volume < 40:
-    print('Achtergrond muxiekje')
-elif 40 <= volume <80:
-    print('Dat is luider!')
-else:
-    print('Wat voor geluid is dit!!')
+def hi(name):
+    if name == 'Ola':
+        print ('Hoi Ola!')
+    elif name == 'Sandra':
+        print ('How are you Sandra?')
+    else:
+        print('Ik ken je niet')
+hi('Lu')
